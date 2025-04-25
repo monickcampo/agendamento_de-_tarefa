d64d0f3e261101d3fb3fb8f1653dea1a9cb78398
monique <moniquecampo2006@gmail.com>
2025-04-25 19:36:50 -0400

    inplementado tela de listar tarefas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,81 @@
-import { View, Text} from 'react-native';
+import { View, Text,StyleSheet, Touchable, TouchableOpacity} from 'react-native';
+import TarefaItem from '../components/TarefaItem';
 
 export default function Home(){
     return (
-        <View>
-            <Text>Home Page</Text>
+        <View style={styles.container}>
+            <View style={styles.cabecalho}>
+              <Text style={styles.titulo}>ABRIL / 2025</Text>  
+              <View style={styles.icone}></View>
+            </View>
+            <View style={styles.boby}>
+              <TarefaItem 
+                 nome="Tarefas 1"
+                 status="a cumprir"
+                 data="20/04/2024"
+                 categoria="Reunião"
+              />
+             <TarefaItem 
+                 nome="Tarefas 2"
+                 status="concluido"
+                 data="20/04/2022"
+                 categoria="Estudo"
+              />
+
+
+            <TouchableOpacity
+                style={styles.botaoAdicionar}
+                onPress={()=>{
+                    alert("wedwe")
+                }}
+            >
+                    <Text style={styles.textoBotaoAdicionar}>+</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    cabecalho: {
+        backgroundColor: 'blue',
+        width:'100%',
+        height: 60,
+        flexDirection: 'row',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    titulo:{
+       color:'white',
+       fontSize:18,
+       fontWeight: 'bold',
+       textAlign:'center'
+    },
+    icone:{
+        backgroundColor:'gray',
+        width: 30,
+        height: 30,
+        borderRadius: 20,
+        position:'absolute',
+        right: 15
+    },
+    body:{
+        flex: 1
+    },
+    botaoAdicionar:{
+        backgroundColor: 'green',
+        width: 30,
+        height:30,
+        borderRadius:20,
+        position: 'absolute',
+        right: 15,
+        bottom: -500
+    },
+    textoBotaoAdicionar:{
+        fontSize: 20,
+        color:'white',
+        textAlign: 'center',
+        bottom: 2
+    }
+
+});
